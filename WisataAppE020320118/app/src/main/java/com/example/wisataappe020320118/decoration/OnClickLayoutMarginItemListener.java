78c541e87f18ffc7b6a0ed0712e3b2788fa1ac8e
unknown <you@example.com>
2023-01-16 10:48:02 -0800

    first commit

---
+++ WisataAppE020320118/app/src/main/java/com/example/wisataappe020320118/decoration/OnClickLayoutMarginItemListener.java
@@ -0,0 +1,11 @@
+package com.example.wisataappe020320118.decoration;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public interface OnClickLayoutMarginItemListener {
+    void onClick(Context context, View v, int position, int spanIndex, RecyclerView.State state);
+}
